c396a9354101dd6f88d84bed798be6d55c96922b
Zilgaro <verner_saaristo@msn.com>
2017-05-08 13:09:25 +0300

    Tehtävä 1 final

--- KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
+++ KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
@@ -1,16 +1,12 @@
 package ohtu.kivipaperisakset;
 
 import java.util.Scanner;
-
 public class Paaohjelma {
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static PelimuotoTehdas pelimuodot;
-    private static Kayttoliittyma kayttis;
-
     public static void main(String[] args) {
-        pelimuodot = new PelimuotoTehdas(scanner);
-        kayttis = new Kayttoliittyma();
+        Scanner scanner = new Scanner(System.in);
+        PelimuotoTehdas pelimuodot = new PelimuotoTehdas(scanner);
+        Kayttoliittyma kayttis = new Kayttoliittyma();
         //pilkotaan
         while (true) {
             kayttis.alkuOhjeistus();
